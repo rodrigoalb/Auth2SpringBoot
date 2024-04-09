7f287e0f90dbe637b00a3216458119bfb32be7fe
rodrigoalb <rodrigoalba-@outlook.com>
2024-04-08 22:47:48 -0300

    BCrypt added

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$tW4I/WqLrm6iYPLSsx9Ia.ytrpzKFc4D.9pz0SF7zlarzDA7IPw5a');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$tW4I/WqLrm6iYPLSsx9Ia.ytrpzKFc4D.9pz0SF7zlarzDA7IPw5a');
 
 INSERT INTO tb_product (name) VALUES ('TV');
 INSERT INTO tb_product (name) VALUES ('Computer');
